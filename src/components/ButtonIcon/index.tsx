b262ea4fce5a416eab4766e4ccc72827661b6b0d
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2021-06-26 11:53:57 -0300

    aula 1 e 2 NLW

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Text, Image, View } from "react-native";
+
+import DicordImg from "../../assets/discord.png";
+import { styles } from "./styles";
+
+type Props = RectButtonProps & {
+  title: string;
+};
+
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <RectButton style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DicordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}> {title}</Text>
+    </RectButton>
+  );
+}
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
+import { styles } from "./styles";
+
+export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignin() {
+    navigation.navigate("Home");
+  }
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`}e organize suas {`\n`}jogatinas
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" onPress={handleSignin} />
+      </View>
+    </View>
+  );
+}
